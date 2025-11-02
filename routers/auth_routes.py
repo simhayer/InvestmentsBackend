eb0492b27c19bb127eb57b0268d521529e6f00bc
shayer23 <shayer@mantech-inc.com>
2025-11-02 16:54:55 -0500

    trying the same site cookie lax

--- routers/auth_routes.py
+++ routers/auth_routes.py
@@ -21,7 +21,8 @@ router = APIRouter()
 # --- Cookie config ---
 AUTH_COOKIE_NAME = os.getenv("AUTH_COOKIE_NAME", "auth_token")
 AUTH_COOKIE_MAX_AGE = 60 * 60 * 24 * 7  # 7 days
-AUTH_COOKIE_SAMESITE = os.getenv("AUTH_COOKIE_SAMESITE", "lax") == "lax"  # 'lax' for locahost, 'none' for prod
+AUTH_COOKIE_SAMESITE = "lax"
+# AUTH_COOKIE_SAMESITE = os.getenv("AUTH_COOKIE_SAMESITE", "lax") == "lax"  # 'lax' for locahost, 'none' for prod
 # Set to False only for local HTTP; must be True on HTTPS
 AUTH_COOKIE_SECURE = os.getenv("AUTH_COOKIE_SECURE", "true").lower() == "true"
 AUTH_COOKIE_DOMAIN = os.getenv("AUTH_COOKIE_DOMAIN", None)  # e.g. ".yourdomain.com"
